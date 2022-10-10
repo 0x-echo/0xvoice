8087ae0d5dcef80dd851d3da385a4f0065e3e055
Airyland <i@mao.li>
2022-10-11 01:00:24 +0800

    add analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -38,6 +38,11 @@ export default defineNuxtConfig({
       href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap'
     }]
   },
+  script: [{
+    src: 'https://analytics.0xecho.com/js/plausible.js',
+    'data-domain': '0xvoice.com',
+    defer: true
+  }],
   buildModules: [
     '@pinia/nuxt',
   ],
